d3a93e0df5ad1b7de999349ecc84694ce5b9f1e5
Gabriel-munizds <gabrielmunizdasilva40@gmail.com>
2023-03-26 11:45:07 -0300

    feat: add others structures

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -6,6 +6,8 @@ import { router } from './routes';
 
 const server = express();
 
+
+server.use(express.json());
 server.use(router);
 
 export{ server };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,17 +1,19 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
 router.get('/teste', (req, res) => {
-	return res.send('Rota funcionando!');
+	return res.send('TESTE');
 });
 
 router.get('/users', (req, res) => {
 	return res.send('<h1>Usuários</h1>'); 
 });
 
-router.get('/hello', (req, res) => {
-	return res.send('Olá, Mundo!'); 
+router.post('/hello', (req, res) => {
+	console.log(req.body);
+	return res.status(StatusCodes.UNAUTHORIZED).json(req.body); 
 });
 
 export { router };
