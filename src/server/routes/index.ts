d6718a1bba4deaedeea34a43fb18ca8640fc3493
Gabriel Muniz <gabrielmunizdasilva40@gmail.com>
2023-04-05 13:02:15 -0300

    teste de conexão com o banco

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,13 +1,34 @@
+import express, { Request, Response } from 'express';
+import mysql from 'mysql2';
+
 import { Router } from 'express';
 
 import { CidadesController } from './../controllers';
 
 const router = Router();
 
+const connection = mysql.createConnection({
+  host: 'containers-us-west-32.railway.app',
+  user: 'root',
+  password: 'EVq54OKSNOt7SL2Ham4Y',
+  database: 'railway',
+  port: 7214
+});
+
 router.get('/', (req, res) => {
 	return res.send('FUNCIONANDO!');
 });
 
-router.post('/cidades', CidadesController.create);
+router.get('/cidades', (req: Request, res: Response) => {
+	const sql = 'SELECT * FROM cidades';
+	connection.query(sql, (err, results) => {
+	  if (err) {
+		console.error(err);
+		res.status(500).send('Erro ao buscar cidades');
+	  } else {
+		res.json(results);
+	  }
+	});
+  });
 
 export { router };
